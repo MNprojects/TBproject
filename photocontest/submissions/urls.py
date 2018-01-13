5ae4a771abecf4ce5e2444cd210de85be5cb4c66
MNprojects <michaelnash.work@gmail.com>
2018-01-13 16:14:03 +0000

    Created photo viewing page and single page for each photo.
    Needs to be edited to allow filtering and show display the photos

--- photocontest/submissions/urls.py
+++ photocontest/submissions/urls.py
@@ -7,6 +7,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index',)
+    path('', views.index, name='index',),
+    path('photos/', views.PhotoListView.as_view(), name='photos'),
+    path('photo/<int:pk>', views.PhotoDetailView.as_view(), name='photo-detail'),
 ]
 
